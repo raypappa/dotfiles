db8cfc8b1609ed8d89a5eb7f05dc96fbe409b67f
Craig Davis <11911367+blade2005@users.noreply.github.com>
2022-12-11 19:48:59 -0800

    Update vim.install.sh

--- vim.install.sh
+++ vim.install.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 base_dir=~/.config/nvim
 
-if [[ -e ~/.config/nvim ]];then
-  echo vim already configured. Exiting.
-  exit 1
-fi
-
 if ! type nvim; then
   if grep -P --quiet '(?<=ID=)debian$' /etc/os-release; then
-    sudo apt-get remove -y --purge vim vim-runtime vim-gnome vim-tiny vim-common vim-gui-common vim-nox
-    sudo apt-get install -y git neovim
+    sudo apt-get remove -y --purge vim*
+    sudo apt-get install -y git
+    wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb
+    sudo apt install ./nvim-linux64.deb
+    rm nvim-linux64.deb
+    sudo update-alternatives --install /usr/local/bin/vim vim $(which nvim) 20
   fi
 fi
 
@@ -22,9 +21,6 @@ if [ ! -d $base_dir/bundle/Vundle.vim ];then
   git clone https://github.com/VundleVim/Vundle.vim.git $base_dir/bundle/Vundle.vim
 fi
 
-vim +PluginInstall +qall
-#
-pushd $base_dir/bundle/plugins/coc.nvim && yarn install --frozen-lockfile; popd
 vim +PluginInstall +qall
 
 pushd $base_dir/bundle/plugins/flake8-vim/ftplugin/python/pycodestyle && git checkout main && git pull; popd
