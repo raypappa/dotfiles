3fbcd058c70e3aa301012d4fe06a7f7e300617f7
Craig Davis <11911367+blade2005@users.noreply.github.com>
2024-07-16 15:23:12 -0700

    add glow, schemastore, and yaml-companion to plugins

---
+++ .config/nvim/lua/custom/plugins/yaml-companion.lua
@@ -0,0 +1,15 @@
+return {
+  'msvechla/yaml-companion.nvim',
+  branch = 'kubernetes_crd_detection',
+  ft = { 'yaml' },
+  dependencies = {
+    'neovim/nvim-lspconfig',
+    'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope.nvim',
+  },
+  config = function(_, opts)
+    local cfg = require('yaml-companion').setup(opts)
+    require('lspconfig')['yamlls'].setup(cfg)
+    require('telescope').load_extension 'yaml_schema'
+  end,
+}
