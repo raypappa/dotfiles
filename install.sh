55620ef75753379e650a800ce42ecd85fc99af60
Craig Davis <11911367+blade2005@users.noreply.github.com>
2024-07-31 15:06:35 -0700

    feat(install.sh): use list of functions to run instead of manually executing each
    Use a list of functions to execute and preface each with a confirm prompt. This will allow me to better see what is executing when.

--- install.sh
+++ install.sh
@@ -4,22 +4,9 @@ set -e
 
 . .install
 
-_install_deps
+for fn in "_install_deps" "_source_bashrc" "_install_cspell" "_install_alacrity_support" "_install_nvshim" "_configure_nvim" "_configure_global_git" "_source_bashrc" "_configure_locale" "_start_ssh_agent";do
+  confirm "Execute $fn" && ${!fn}
+done
 
-. "$HOME/.bashrc"
-
-_install_cspell
-_install_alacrity_support
-_install_nvshim
-
-_configure_nvim
-_configure_global_git
 echo "krew plugin for df-pv available but not installed by default"
-
-
-. ~/.bashrc
-
-_configure_locale
-_start_ssh_agent
-
 echo Please restart terminal.
