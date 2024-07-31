79aa235a704bbbd6c28a19b6d4069c0332b851dc
Craig Davis <11911367+blade2005@users.noreply.github.com>
2024-07-31 15:29:09 -0700

    fix(install): nvm needs to be installed independently

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+. $HOME/.bashrc
+
 set -e
 
 . .install
 
-for fn in "_install_deps" "_source_bashrc" "_install_cspell" "_install_alacrity_support" "_install_nvshim" "_configure_nvim" "_configure_global_git" "_source_bashrc" "_configure_locale" "_start_ssh_agent";do
+for fn in "_install_deps" "_install_nvm" "_install_cspell" "_install_alacrity_support" "_install_nvshim" "_configure_nvim" "_configure_global_git" "_configure_locale" "_start_ssh_agent";do
   confirm "Execute $fn" && ${fn}
 done
 
