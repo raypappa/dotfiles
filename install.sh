1089bc064218d381c94c3caa2c6cd48dd3b9315a
Craig Davis <11911367+blade2005@users.noreply.github.com>
2024-07-31 12:59:52 -0700

    feat(install.sh): order the install order by type

--- install.sh
+++ install.sh
@@ -6,36 +6,46 @@ _install_deps
 
 . ~/.bashrc
 
-_install_alacrity_support
-_install_polyglot_prompt
+# Programming languages/language version management
+_install_goenv
 _install_rbenv
 _install_pyenv
-_install_pipx
-_install_poetry
 _install_nvm
 _install_nvshim
-_install_tfenv
 _install_rust
-_install_aws_azure_login
+
+# Tool version management
+_install_tfenv
+
+# Package managers
+_install_poetry
+_install_pipx
+_install_pnpm
+
+# Tools
 _install_cspell
 _install_aws
-_install_docker
-_install_lazydocker
-_install_vim
-_configure_nvim
 _install_tflint
 _install_sam_cli
 _install_taskfile_cli
-_configure_global_git
-_install_gh_cli
 _install_kubectl
-_install_pnpm
 _install_glow
 _install_aws_vault
 _install_ripgrep
 _install_fd
 _install_ctags
 _install_vale
+_install_aws_azure_login
+_install_gh_cli
+_install_vim
+
+# Others
+_install_polyglot_prompt
+_install_alacrity_support
+_install_docker
+_install_lazydocker
+_configure_nvim
+_configure_global_git
 echo "krew plugin for df-pv available but not installed by default"
 
 
