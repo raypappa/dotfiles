e74e1bb17b8f3a1ddbce7ad155937d0698715c7c
Craig Davis <11911367+blade2005@users.noreply.github.com>
2024-07-31 15:11:38 -0700

    fix(install.sh): don't dereference

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ set -e
 . .install
 
 for fn in "_install_deps" "_source_bashrc" "_install_cspell" "_install_alacrity_support" "_install_nvshim" "_configure_nvim" "_configure_global_git" "_source_bashrc" "_configure_locale" "_start_ssh_agent";do
-  confirm "Execute $fn" && ${!fn}
+  confirm "Execute $fn" && ${fn}
 done
 
 echo "krew plugin for df-pv available but not installed by default"
