b012467c3d23f49010f2756d9b106ce658fb8a1c
Craig Davis <crdavis@salucro.com>
2025-02-28 10:05:51 -0800

    update github copilot config

--- .config/nvim/lua/custom/plugins/github-copilot.lua
+++ .config/nvim/lua/custom/plugins/github-copilot.lua
@@ -5,6 +5,8 @@ return {
     event = 'InsertEnter',
     config = function()
       require('copilot').setup {
+        suggestion = { enabled = false },
+        panel = { enabled = false },
         filetypes = {
           yaml = true,
         },
